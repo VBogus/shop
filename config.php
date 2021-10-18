c917a4df0f8d8c0b91e226bff04747a59d7b6edb
VBogus <vbogus@ymail.com>
2021-10-18 16:31:23 +0300

    setting

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+defined('VG_ACCESS') or die('Access denied');
+const SITE_URL = 'HTTP^//SHOP';
+const PATH = '/';
+const HOST ='localhost';
+const user = 'root';
+const pass = '';
+const DB_NAME = "im"
--- index.php
+++ index.php
@@ -1,1 +1,8 @@
 <?php
+
+define('VG_ACCESS', true);
+
+header('content-Type:text/html:charset=utf-8');
+session_start();
+require_once ('config.php');
+require_once ('core/base/settings/internal_settings.php');
